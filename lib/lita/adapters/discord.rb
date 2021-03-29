90ccd38f539ac842cbbb59f8ecf43f2ac85667c2
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2021-03-29 00:59:06 -0400

    wip

--- lib/lita/adapters/discord.rb
+++ lib/lita/adapters/discord.rb
@@ -1,7 +1,68 @@
+require 'discordrb'
+
 module Lita
   module Adapters
     class Discord < Adapter
-      # insert adapter code here
+
+      config :token,    type: String, required: true
+      config :channels, type: Array
+
+      def initialize(robot)
+        super
+        @bot = ::Discordrb::Bot.new token: config.token
+      end
+
+      def run
+        # register message handler and convert to Lita data model
+        @bot.message do |event|
+          user = Lita::User.find_by_name(event.user.username)
+          user = Lita::User.create(event.user.username) unless user
+          source = Lita::Source.new(user: user, room: event.channel)
+          message = Lita::Message.new(robot, event.message.content, source)
+          robot.receive(message)
+        end
+
+        # The `mention` event is called if the bot is *directly mentioned*, i.e. not using a role mention or @everyone/@here.
+        # @bot.mention do |event|
+        #
+        #
+        #   # The `pm` method is used to send a private message (also called a DM or direct message) to the user who sent the
+        #   # initial message.
+        #   event.user.pm('You have mentioned me!')
+        # end
+
+        # Run the bot in another thread in the background:
+        @bot.run(true)
+
+        config.channels.each do |channel|
+          @bot.send_message(channel, 'Bot is now active!')
+        end
+
+        # Join the bot's thread back with the main thread:
+        @bot.join
+      end
+
+      def send_messages(target, messages)
+        messages.each do |message|
+          @bot.send_message(target.room || target.user, message)
+        end
+      end
+
+      def shut_down
+        # TODO
+      end
+
+      def set_topic(target, topic)
+        # TODO
+      end
+
+      def join(channel_id)
+        # TODO
+      end
+
+      def part(channel_id)
+        # TODO
+      end
 
       Lita.register_adapter(:discord, self)
     end
--- lita-discord.gemspec
+++ lita-discord.gemspec
@@ -2,11 +2,11 @@ Gem::Specification.new do |spec|
   spec.name          = "lita-discord"
   spec.version       = "0.1.0"
   spec.authors       = ["Maxime 'biximilien' Gauthier"]
-  spec.email         = ["maxime.gauthier88@gmail.com"]
-  spec.description   = "TODO: Add a description"
-  spec.summary       = "TODO: Add a summary"
-  spec.homepage      = "TODO: Add a homepage"
-  spec.license       = "TODO: Add a license"
+  spec.email         = ["maxime@gauthier.software"]
+  spec.description   = "A Discord adapter for Lita."
+  spec.summary       = "A Discord adapter for Lita."
+  spec.homepage      = "https://github.com/biximilien/lita-discord"
+  spec.license       = "MIT"
   spec.metadata      = { "lita_plugin_type" => "adapter" }
 
   spec.files         = `git ls-files`.split($/)
@@ -15,8 +15,9 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "lita", ">= 4.8"
+  spec.add_runtime_dependency "discordrb", "~> 3.4"
 
-  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "bundler"
   spec.add_development_dependency "pry-byebug"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rack-test"
